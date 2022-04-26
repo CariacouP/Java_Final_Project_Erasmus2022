cd7e372db321954cd3fa7083e400630d7e73e4f2
Luca Bankofski <luca.bankofski@edu.ece.fr>
2022-04-26 15:11:28 +0300

    ajout d'adresse

---
+++ src/Hierarchie/Adresse.java
@@ -0,0 +1,38 @@
+package Hierarchie;
+
+public class Adresse {
+	int number;
+	String street;
+	String town;
+	String zipCode;
+	String Country;
+	
+	Adresse(){
+		
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getTown() {
+		return town;
+	}
+	public void setTown(String town) {
+		this.town = town;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+}
---
+++ src/Hierarchie/Student.java
@@ -0,0 +1,10 @@
+package Hierarchie;
+
+public class Student {
+	String name;
+	String Surname;
+	String Field_Of_Studie;
+	Adresse Adresse;
+	int id_Student;
+
+}
