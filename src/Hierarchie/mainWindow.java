04a1cca7e24340713e148405378a1c12ce53d71c
Celiancaf <celian.cafournet@edu.ece.fr>
2022-05-03 15:26:50 +0300

    M-à-j de la page d'acceuil
    Amélioration de l'agencement des différents boutons sur la page d'acceuil

--- src/Hierarchie/mainWindow.java
+++ src/Hierarchie/mainWindow.java
@@ -19,7 +19,7 @@ public class mainWindow extends JFrame implements ActionListener{
 		this.setSize(960,540); //set the x-dimension and the y-dimension of the this
 		Image icon = Toolkit.getDefaultToolkit().getImage("ressources/Logo S.png"); // create an image icon
 		this.setIconImage(icon); // replace the standard icon of the window
-		this.setLayout(new FlowLayout()); // set a Layout for all th window
+		this.setLayout(new GridLayout(4,1)); // set a Layout for all the window
 		
 		JLabel label = new JLabel(); // create a new label
 		ImageIcon image = new ImageIcon("ressources/Logo Student.png"); // add an image to the label
@@ -36,39 +36,45 @@ public class mainWindow extends JFrame implements ActionListener{
 		ListingS.setText("List students");
 		ListingS.setForeground(Color.WHITE);
 		ListingS.setBackground(new Color(66,133,244));
-		ListingS.setPreferredSize(new Dimension(100,20));
+		ListingS.setPreferredSize(new Dimension(300,20));
 		
 		addS = new JButton(); // create a new button
-		addS.setText("Add students"); //set the text inside the button
+		addS.setText("Add a new student"); //set the text inside the button
 		addS.setForeground(Color.WHITE); //change the text color of the button
 		addS.setBackground(new Color(66,133,244)); //change the background color of the button
-		addS.setPreferredSize(new Dimension(100,20)); // set the size of the button
+		addS.setPreferredSize(new Dimension(300,20)); // set the size of the button
 		
 		JPanel headerPanel = new JPanel(); // create a headerPanel
 		headerPanel.setLayout(new FlowLayout());
 		headerPanel.add(label); // add the label to the headerPanel
-		headerPanel.setBackground(Color.WHITE);//set Background color of the headerPanel
+		//headerPanel.setBackground(Color.WHITE);//set Background color of the headerPanel
 		headerPanel.setPreferredSize(new Dimension(960,120)); //set the place of the headerPanel
 		
 		JPanel searchBar = new JPanel(); //create a searchBar Panel
 		searchBar.setLayout(new FlowLayout());
 		searchBar.add(textfield); // add the textField to the middle Panel
 		searchBar.add(bSearch); // add the Search button
-		searchBar.setBackground(Color.WHITE);//set Background color of the headerPanel
+		//searchBar.setBackground(Color.WHITE);//set Background color of the headerPanel
 		
 		
-		JPanel Buttons = new JPanel(); // add a new Buttons Panel
-		Buttons.setLayout(new GridLayout(2,1)); // set a grid layout for the two buttons
-		Buttons.add(ListingS); //add the button Listing S to the layout
-		Buttons.add(addS); //add the button addS to the layout
+		JPanel ButtonList = new JPanel(); // add a new Buttons Panel
+		ButtonList.setLayout(new FlowLayout()); // set a grid layout for the two buttons
+		ButtonList.add(ListingS); //add the button Listing S to the layout
+		//ButtonList.setBackground(Color.WHITE);//set Background color of the headerPanel
+		
+		JPanel ButtonAdd = new JPanel(); // add a new Buttons Panel
+		ButtonAdd.setLayout(new FlowLayout()); // set a grid layout for the two buttons
+		ButtonAdd.add(addS); //add the button addS to the layout
+		//ButtonAdd.setBackground(Color.WHITE);//set Background color of the headerPanel
 		
 		
 		this.add(headerPanel); // add the header panel to the window
 		this.add(searchBar); // add the searchbar panel to the window
-		this.add(Buttons); // add the Buttons panel to the window
+		this.add(ButtonList); // add the Buttons panel to the window
+		this.add(ButtonAdd); // add the Buttons panel to the window
 		
 		this.setResizable(false); // prevent this from being resize
-		this.getContentPane().setBackground(new Color(255,255,255)); //change the color of the background
+		//this.getContentPane().setBackground(new Color(255,255,255)); //change the color of the background
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
 		this.setVisible(true); //make the this visible
 	}
@@ -76,9 +82,10 @@ public class mainWindow extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		/*if(e.getSource()==bSearch) {
-			System.out.println("hello");
-		}*/
+		if(e.getSource()==bSearch) {
+			/*this.getContentPane().removeAll();
+			this.repaint();*/
+		}
 	}
 	
 	
