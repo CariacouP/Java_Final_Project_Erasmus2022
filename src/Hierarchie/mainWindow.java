0efcb3a86f8ee3fcd7f1e686ffe173a9afcbba74
CariacouP <bankofskiluca@yahoo.fr>
2022-05-04 19:25:47 +0300

    travail a 2

--- src/Hierarchie/mainWindow.java
+++ src/Hierarchie/mainWindow.java
@@ -12,6 +12,12 @@ public class mainWindow extends JFrame implements ActionListener{
 	JButton ListingS;
 	JButton addS;
 	JTextField textfield;
+	Student[] students= new Student[100];
+	
+	JPanel ButtonAdd = new JPanel();
+	JPanel ButtonList = new JPanel();
+	JPanel searchBar = new JPanel();
+	JPanel headerPanel = new JPanel();
 	
 	mainWindow(){
 		
@@ -43,30 +49,32 @@ public class mainWindow extends JFrame implements ActionListener{
 		addS.setForeground(Color.WHITE); //change the text color of the button
 		addS.setBackground(new Color(66,133,244)); //change the background color of the button
 		addS.setPreferredSize(new Dimension(300,20)); // set the size of the button
+		addS.addActionListener(this);
 		
-		JPanel headerPanel = new JPanel(); // create a headerPanel
+		 // create a headerPanel
 		headerPanel.setLayout(new FlowLayout());
 		headerPanel.add(label); // add the label to the headerPanel
 		headerPanel.setBackground(Color.WHITE);//set Background color of the headerPanel
 		headerPanel.setPreferredSize(new Dimension(960,120)); //set the place of the headerPanel
 		
-		JPanel searchBar = new JPanel(); //create a searchBar Panel
+		 //create a searchBar Panel
 		searchBar.setLayout(new FlowLayout());
 		searchBar.add(textfield); // add the textField to the middle Panel
 		searchBar.add(bSearch); // add the Search button
 		searchBar.setBackground(Color.WHITE);//set Background color of the headerPanel
 		
 		
-		JPanel ButtonList = new JPanel(); // add a new Buttons Panel
+		 // add a new Buttons Panel
 		ButtonList.setLayout(new FlowLayout()); // set a grid layout for the two buttons
 		ButtonList.add(ListingS); //add the button Listing S to the layout
 		ButtonList.setBackground(Color.WHITE);//set Background color of the headerPanel
 		
-		JPanel ButtonAdd = new JPanel(); // add a new Buttons Panel
+		 // add a new Buttons Panel
 		ButtonAdd.setLayout(new FlowLayout()); // set a grid layout for the two buttons
 		ButtonAdd.add(addS); //add the button addS to the layout
 		ButtonAdd.setBackground(Color.WHITE);//set Background color of the headerPanel
 		
+	
 		
 		this.add(headerPanel); // add the header panel to the window
 		this.add(searchBar); // add the searchbar panel to the window
@@ -86,7 +94,58 @@ public class mainWindow extends JFrame implements ActionListener{
 			this.getContentPane().removeAll();
 			this.repaint();
 		}
+		if(e.getSource()==addS) {
+			//this.getContentPane().removeAll();
+			this.remove(headerPanel);
+			this.remove(ButtonAdd);
+			this.remove(ButtonList);
+			this.remove(searchBar);
+			//this.repaint();
+			createNewStudent();
+			
+		}
+		
 	}
+	
+	public void createNewStudent() {
+		
+		if (students.length<100) {
+			int i =students.length;
+			Student newStd =new Student();
+			newStd.windowInputDataStudent();
+								
+					if (newStd.typeOfStudie=="Full time student") {
+						//this.removeAll();
+						
+						FullTimeStd newFullStd= new FullTimeStd(newStd.name,newStd.Surname,newStd.Field_Of_Studie,newStd.Adresse,newStd.id_Student);
+						
+						newFullStd.windowInputFullTimeStd();
+						System.out.println("3");
+						System.out.println(newFullStd.toString());
+						students[i]=newFullStd;
+					}
+					
+					else if (newStd.typeOfStudie=="Part time Student") {
+						PartTimeStd newPartStd= new PartTimeStd(newStd.name,newStd.Surname,newStd.Field_Of_Studie,newStd.Adresse,newStd.id_Student);
+						
+						newPartStd.windowInputPartTimeStd();
+						System.out.println(newPartStd.toString());
+						students[i]=newPartStd;
+						
+					}
+					
+					else if (newStd.typeOfStudie=="Erasmus Student") {
+						ErasmusStd newErasmusStd= new ErasmusStd(newStd.name,newStd.Surname,newStd.Field_Of_Studie,newStd.Adresse,newStd.id_Student);
+						
+						newErasmusStd.windowInputErasmusStd();
+						System.out.println(newErasmusStd.toString());
+						students[i]=newErasmusStd;
+					}
+			}
+		
+	}
+	
+	
 	
 	
 }
