6d0ad79aa66ad60a847252b928580be0c5ab26c6
Luca Bankofski <luca.bankofski@edu.ece.fr>
2022-05-04 12:08:46 +0300

    v0.4.0.1
    while loop commented, codes work but no registration in an arry

--- src/Hierarchie/main_StudentH.java
+++ src/Hierarchie/main_StudentH.java
@@ -16,36 +16,47 @@ public class main_StudentH extends JFrame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Student newStd =new Student();
-		
 		newStd.windowInputDataStudent();
-				
-			
-			if (newStd.typeOfStudie=="Full time student") {
-				//this.removeAll();
-				
-				FullTimeStd newFullStd= new FullTimeStd(newStd.name,newStd.Surname,newStd.Field_Of_Studie,newStd.Adresse,newStd.id_Student);
-				
-				newFullStd.windowInputFullTimeStd();
-				System.out.println("3");
-				System.out.println(newFullStd.toString());
-				
-			}
-			
-			else if (newStd.typeOfStudie=="Part time Student") {
-				PartTimeStd newPartStd= new PartTimeStd(newStd.name,newStd.Surname,newStd.Field_Of_Studie,newStd.Adresse,newStd.id_Student);
-				
-				newPartStd.windowInputPartTimeStd();
-				System.out.println(newPartStd.toString());
-			}
-			
-			else if (newStd.typeOfStudie=="Erasmus Student") {
-				ErasmusStd newErasmusStd= new ErasmusStd(newStd.name,newStd.Surname,newStd.Field_Of_Studie,newStd.Adresse,newStd.id_Student);
-				
-				newErasmusStd.windowInputErasmusStd();
-				System.out.println(newErasmusStd.toString());
-			}
-		//System.out.println(newStd.toString());
+		//int i=0;
+		//Student[] students= new Student[100];
 		
+		/*
+		while (i<students.length) {
+			Student newStd =new Student();
+			newStd.setId_Student(i);
+			newStd.windowInputDataStudent();
+			*/
+					
+				
+				if (newStd.typeOfStudie=="Full time student") {
+					//this.removeAll();
+					
+					FullTimeStd newFullStd= new FullTimeStd(newStd.name,newStd.Surname,newStd.Field_Of_Studie,newStd.Adresse,newStd.id_Student);
+					
+					newFullStd.windowInputFullTimeStd();
+					System.out.println("3");
+					System.out.println(newFullStd.toString());
+					//students[i]=newFullStd;
+				}
+				
+				else if (newStd.typeOfStudie=="Part time Student") {
+					PartTimeStd newPartStd= new PartTimeStd(newStd.name,newStd.Surname,newStd.Field_Of_Studie,newStd.Adresse,newStd.id_Student);
+					
+					newPartStd.windowInputPartTimeStd();
+					System.out.println(newPartStd.toString());
+					//students[i]=newPartStd;
+					
+				}
+				
+				else if (newStd.typeOfStudie=="Erasmus Student") {
+					ErasmusStd newErasmusStd= new ErasmusStd(newStd.name,newStd.Surname,newStd.Field_Of_Studie,newStd.Adresse,newStd.id_Student);
+					
+					newErasmusStd.windowInputErasmusStd();
+					System.out.println(newErasmusStd.toString());
+					//students[i]=newErasmusStd;
+				}
+			//i++;
+		//}
 	}
-
 }
+
